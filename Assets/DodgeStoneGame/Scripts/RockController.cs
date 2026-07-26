760decf7cdedb9bb23ac4db26a3367f2479660cc
Vladimir Strogonov <nice.strogonov@inbox.ru>
2026-07-26 18:25:54 +0300

    feat: завершил игру

---
+++ Assets/DodgeStoneGame/Scripts/RockController.cs
@@ -0,0 +1,88 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class RockController : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    public InputAction moveAction;
+    public Vector2 moveInput;
+    private float lastDirection = 1;
+
+    private Animator anim;
+    private bool alive = true;
+
+    AudioSource source;
+    public AudioClip clip;
+
+    void Start()
+    {
+        moveAction.Enable();
+        anim = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
+
+    }
+
+    void Update()
+    {
+
+
+        Move();
+
+
+
+
+    }
+
+
+
+    void Move()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+        if (moveInput.x > 0)
+        {
+
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+
+
+        }
+
+        if (moveInput.x < 0)
+        {
+
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+
+
+        }
+
+        if (transform.position.x < -4)
+        {
+            transform.position = new Vector3(-4, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 20)
+        {
+            transform.position = new Vector3(20, transform.position.y, transform.position.z);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+
+        }
+
+        if (other.gameObject.CompareTag("Runner") )
+        {
+            Debug.Log(1);
+
+
+        }
+    }
+
+
+}
---
+++ Assets/DodgeStoneGame/Scripts/RunnerController.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+public class RunnerController : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    public InputAction moveAction;
+    public Vector2 moveInput;
+    private float lastDirection = 1;
+
+    private Animator anim;
+    private bool alive = true;
+
+    AudioSource source;
+    public AudioClip clip;
+
+    void Start()
+    {
+        moveAction.Enable();
+        anim = GetComponent<Animator>();
+        source = GetComponent<AudioSource>();
+
+    }
+
+    void Update()
+    {
+
+        if (alive)
+        {
+            Move();
+        }
+
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Rock") && alive && other.gameObject.transform.position.y > 2.0f)
+        {
+            alive = false;
+            anim.SetBool("death", true);
+            source.PlayOneShot(clip);
+
+
+        }
+    }
+
+    void Move()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+        if (moveInput.x > 0)
+        {
+            if (lastDirection < 0)
+            {
+                transform.Rotate(new Vector3(0, 180, 0));
+
+            }
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+
+        }
+
+        if (moveInput.x < 0)
+        {
+            if (lastDirection > 0)
+            {
+                transform.Rotate(new Vector3(0, -180, 0));
+
+            }
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+
+        }
+        if (moveInput.x != 0)
+        {
+            lastDirection = moveInput.x;
+        }
+
+        if (transform.position.x < -4)
+        {
+            transform.position = new Vector3(-4, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 20)
+        {
+            transform.position = new Vector3(20, transform.position.y, transform.position.z);
+        }
+
+    }
+
+
+}
